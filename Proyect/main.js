1a0e63a3f5f674e3a00f1f8960c02932faeaa51a
scarlett-gm <scarlettmartinez.08893@gmail.com>
2025-06-18 22:47:12 -0600

    actu

--- Proyect/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const logo = document.getElementById("logo");
-    logo.addEventListener("click", function () {
-        location.reload();
-    });
-
-    // Cambiar la ruta de la URL sin recargar la página
-    history.pushState(null, "", "/simulador");
-});
